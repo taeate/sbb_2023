4af72433162d4fec5c0dc448e729e8c661e1d5e1
taeate <yes3144@nate.com>
2023-03-20 14:52:21 +0900

    Question,Answer Entity create

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id //primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto increment
+    private Integer id;
+
+    @Column(length = 200) // TEXT
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+}
