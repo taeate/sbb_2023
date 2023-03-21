e83f5fabaf0d1cc1f4961e2e1389b85cb3747e04
taeate <yes3144@nate.com>
2023-03-21 15:54:37 +0900

    각 테스트 데이터 생성 조회 수정 삭제

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -25,7 +26,7 @@ public class Question {
     private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    private List<Answer> answerList = new ArrayList<>();
     public void addAnswer(Answer a) {
         a.setQuestion(this);
         answerList.add(a);
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,26 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
-
+	@Autowired
 	private AnswerRepository answerRepository;
-
+	@BeforeEach
 	@Test
-	void testJpa() {
+	void beforeEach() {
+
+		answerRepository.deleteAll();
+		answerRepository.clearAutoIncrement();
+		// 모든 데이터 삭제
+		questionRepository.deleteAll();
+
+		// 흔적삭제(다음번 INSERT 때 id가 1번으로 설정되도록)
+		questionRepository.clearAutoIncrement();
+
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -38,7 +49,6 @@ class SbbApplicationTests {
 		q2.addAnswer(a1);
 		a1.setCreateDate(LocalDateTime.now());
 		answerRepository.save(a1);
-
 
 	}
 	@Test
@@ -94,5 +104,68 @@ class SbbApplicationTests {
 		 Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
 		 assertEquals(1, q.getId());
 	 }
+	  /*
+    SQL
+    SELECT *
+    FROM question
+    WHERE subject = 'sbb가 무엇인가요?'
+    AND content = 'sbb에 대해서 알고 싶습니다.'
+    */
+	  @Test
+	  @DisplayName("findBySubjectAndContent")
+	  void t005() {
+		  Question q = questionRepository.findBySubjectAndContent(
+				  "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+		  );
+		  assertEquals(1,q.getId());
+	  }
+	  /*
+    SQL
+    UPDATE
+        question
+    SET
+        content = ?,
+        create_date = ?,
+        subject = ?
+    WHERE
+        id = ?
+    */
+	  @Test
+	  @DisplayName("데이터 수정하기")
+	  void t006() {
+		  Optional<Question> oq = questionRepository.findById(1);
+		  assertTrue(oq.isPresent());
+		  Question q = oq.get();
+		  q.setSubject("수정된 제목");
+		  questionRepository.save(q);
+	  }
+	   /*
+    SQL
+    DELETE
+    FROM
+        question
+    WHERE
+        id = ?
+    */
+	   @Test
+	   @DisplayName("데이터 삭제하기")
+	   void t007() {
+		   // questionRepository.count()
+		   // SQL : SELECT COUNT(*) FROM question;
+		   assertEquals(2, questionRepository.count());
+		   Optional<Question> oq = questionRepository.findById(1);
+		   assertTrue(oq.isPresent());
+		   Question q = oq.get();
+		   questionRepository.delete(q);
+		   assertEquals(1, questionRepository.count());
+	   }
+	@Test
+	@DisplayName("답변 조회하기")
+	void t010() {
+		Optional<Answer> oa = answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
 
 }
